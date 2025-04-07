f889181df94311c17661599eca638b5f43f17c8a
Alvar Quezada <alquezada._@hotmail.com>
2025-04-07 12:59:48 -0600

    commit

--- uptask_backend/src/server.ts
+++ uptask_backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import morgan from 'morgan'
 import {corsConfig} from './config/cors'
 import { connectBD } from './config/db'
 import projectRoutes from './routes/ProjectRoutes'
@@ -11,7 +12,10 @@ connectBD()//conectando a base de datos
 const app = express()//inicializando express
 app.use(cors(corsConfig))//Habilitando cors
 
-app.use(express.json())//Habilitando el uso de json
+//Logging
+app.use(morgan('dev'))//Habilitando el uso de morgan para logging
+
+app.use(express.json())//Leer datos de formularios
 
 //Rutas
 app.use('/api/projects', projectRoutes)
