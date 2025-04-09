4a7363be33515d2b7c4c0aa60b787b191b83003a
Alvar Quezada <alquezada._@hotmail.com>
2025-04-08 18:08:42 -0600

    commit

--- uptask_frontend/src/views/projects/EditProjectView.tsx
+++ uptask_frontend/src/views/projects/EditProjectView.tsx
@@ -6,13 +6,11 @@ import { Navigate, useParams } from "react-router-dom"
 export default function EditProjectView() {
     const params = useParams()
     const projectId = params.projectId! // ! es una forma de decirle a typescript que no es undefined
-
     const { data, isLoading, isError } = useQuery({
         queryKey: ['editProject', projectId],
         queryFn: () => getProjectById(projectId),
         retry: false
     })
-
     if (isLoading) return 'Cargando...'
     if (isError) return <Navigate to="/404" />
     if (data) return <EditProjectForm data={data} projectId={projectId}/>
